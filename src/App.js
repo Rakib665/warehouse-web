638d724004af0c372b20720b26d69bffe6f9c11b
Rakib665 <islammdr44@gmail.com>
2022-05-08 17:52:53 +0600

    after create login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 
 function App() {
   return (
     <div >
-     <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+
     </div>
   );
 }
--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <Navbar bg="light" variant="light">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand as={Link} to='/'>Navbar</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link as={Link} to='/home'>Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link as={Link} to='/login'>Login</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
