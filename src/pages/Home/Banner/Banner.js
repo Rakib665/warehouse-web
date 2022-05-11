36f919d3490f307c51caedecaffd4d12b3de0ac7
Rakib665 <islammdr44@gmail.com>
2022-05-12 02:17:11 +0600

    finished

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -9,25 +9,21 @@ const Banner = () => {
                 <Carousel.Item>
                     <img 
                         className="carousel-img d-block w-100"
-                        src="https://i.ibb.co/F89TYhX/banner1.jpg"
+                        src="https://i.ibb.co/Km0fQDJ/banner2.jpg"
+
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="carousel-img d-block w-100"
-                        src="https://i.ibb.co/Km0fQDJ/banner2.jpg"
+                        src="https://i.ibb.co/F89TYhX/banner1.jpg"
+
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -36,10 +32,7 @@ const Banner = () => {
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
             </Carousel>
         </>
